27dc9bad1f6baf5ca351e20f2fa9ff3f5699aa3e
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-22 15:56:11 +0200

    Ejercicio 6 operaciones js unidad 1

--- UD1/06_math_array/main.js
+++ UD1/06_math_array/main.js
@@ -4,10 +4,35 @@
 // El elemento más pequeño
 // La media de los elementos
 
-
+let suma = 0;
 
 function doCalculation(array) {
+    let mayor = array[0];
+    let menor= array[0];
+    for( let i = 0; i < array.length; i++){
+        suma = suma + array[i]; 
+    }
+    console.log ('la suma es: '+ suma )
+    media = suma/array.length; 
+    console.log ('la media es: '+ media )
+   
+    suma = 0;
 
+    for( let i = 0; i < array.length; i++){
+        if (array[i] > mayor) {
+            mayor = array[i];
+        }
+    }
+    console.log ('Mayor es el: '+ mayor )
+    mayor = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < menor) {
+            menor = array[i];
+        }
+    }
+    console.log ('Menor es el: '+ menor )
+    menor = Infinity;
 
 }
 
